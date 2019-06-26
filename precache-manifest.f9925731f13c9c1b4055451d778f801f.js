040922f9d2dcc93086e8ae64168779e30a0f6832
pouyanfarzad <pouyanfarzad@gmail.com>
2019-06-26 16:30:39 +0430

    Updates

---
+++ precache-manifest.f9925731f13c9c1b4055451d778f801f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e63a2c28fe6bf45f0970bddec0ca408",
+    "url": "/maktabkhoone-resume/index.html"
+  },
+  {
+    "revision": "7d30839aa3ff1d103a1d",
+    "url": "/maktabkhoone-resume/static/css/main.f4d6a06e.chunk.css"
+  },
+  {
+    "revision": "da4a88ac8d1dd69daebb",
+    "url": "/maktabkhoone-resume/static/js/2.33655aa9.chunk.js"
+  },
+  {
+    "revision": "7d30839aa3ff1d103a1d",
+    "url": "/maktabkhoone-resume/static/js/main.b4a9a9b9.chunk.js"
+  },
+  {
+    "revision": "b1689e7ccd28ad118f22",
+    "url": "/maktabkhoone-resume/static/js/runtime~main.65c4976a.js"
+  }
+]);
